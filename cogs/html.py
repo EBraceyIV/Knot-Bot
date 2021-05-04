a0af0ba4ff0b5398bda19fab1d424b8d84581be4
EBraceyIV <dbraceyiv@me.com>
2021-05-04 12:01:14 -0400

    Streamlining and cleanup of the get_html function, with added helper function

--- cogs/html.py
+++ cogs/html.py
@@ -4,31 +4,35 @@ import requests
 
 # Get bracelet HTML
 def get_html(bracelet_id):
-    #  Converts the input to a string for clarity in use later,
-    #  if the command runs then we know it's an int as the input should be
-    bracelet_id = str(bracelet_id)
-    # Check both url options, normal and alpha
-    urls = ["https://www.braceletbook.com/patterns/normal/" + bracelet_id + "/",
-            "https://www.braceletbook.com/patterns/alpha/" + bracelet_id + "/"]
+    # For potential use in scraping variations at some point
     # urls = ["https://www.braceletbook.com/variations/normal/" + bracelet_id + "/",
     #         "https://www.braceletbook.com/variations/alpha/" + bracelet_id + "/"]
+
     # Using the exception given by get() in requests, determine if the pattern is a normal or an alpha
     try:
         # Get the page data if the pattern is a normal
-        requests.get(urls[0]).raise_for_status()
-        pattern_info = requests.get(urls[0])
-        url = urls[0]
-        style = "This is a normal pattern created by "
-    except Exception as eNormal:  # Pattern is not a normal
+        requests.get("https://www.braceletbook.com/patterns/normal/" + bracelet_id + "/").raise_for_status()
+        pattern_info, url, style = html_alphanorm(bracelet_id, "normal")
+    except requests.exceptions.HTTPError:  # Pattern is not a normal
         try:
             # Get the page data if the pattern is an alpha
-            requests.get(urls[1]).raise_for_status()
-            pattern_info = requests.get(urls[1])
-            url = urls[1]
-            style = "This is an alpha pattern created by "
-        except Exception as eAlpha:  # Pattern can't be found
+            requests.get("https://www.braceletbook.com/patterns/alpha/" + bracelet_id + "/").raise_for_status()
+            pattern_info, url, style = html_alphanorm(bracelet_id, "alpha")
+        except requests.exceptions.HTTPError:  # Pattern can't be found
             return False, None, None, None
+    # Return HTML data for pattern information, what style the pattern is, and the pattern url
     return True, pattern_info, style, url
+
+
+# HTML scraping helper function to handle normals vs alphas
+def html_alphanorm(bracelet_id, normal_or_alpha):
+    # Pattern URL built based on the pattern style
+    url = "https://www.braceletbook.com/patterns/" + normal_or_alpha + "/" + bracelet_id + "/"
+    # The HTML data for the pattern requested
+    pattern_info = requests.get(url)
+    # Flavor text to describe the pattern style to be used in the output
+    style = "This is a " + normal_or_alpha + " pattern created by "
+    return pattern_info, url, style
 
 
 # Grab info about the crafter who made the bracelet
